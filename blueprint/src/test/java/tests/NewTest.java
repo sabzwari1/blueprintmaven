77a1d9e9bd6785df223933ded95a6997307847ac
sabzwari <umair.sabzwari@gmail.com>
2018-11-27 14:19:45 -0500

    Test Commit

---
+++ blueprint/src/test/java/tests/NewTest.java
@@ -0,0 +1,33 @@
+
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class NewTest {
+
+	public WebDriver driver;
+
+	@Test
+	public void openMyBlog() {
+		driver.get("https://www.softwaretestingmaterial.com/");
+	}
+
+	@BeforeClass
+	public void beforeClass() {
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Umair\\eclipse-workspace\\datadriven\\src\\test\\resources\\drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+}
